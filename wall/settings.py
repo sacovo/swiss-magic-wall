5e856f4d50648316f4dbad2557b42a53a1d21504
Sandro Covo <sandro@sandrocovo.ch>
2020-12-05 18:20:23 +0100

    add formated wall

--- wall/settings.py
+++ wall/settings.py
@@ -16,105 +16,113 @@ from django.utils.translation import gettext_lazy as _
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'atxj8fqigbwhm2av&)0vs#zi+h7h-q7m9o9le*x4mzzuwj(yv1'
+SECRET_KEY = "atxj8fqigbwhm2av&)0vs#zi+h7h-q7m9o9le*x4mzzuwj(yv1"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
 INSTALLED_APPS = [
     "jazzmin",
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django_celery_beat',
-    'django_celery_results',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "django_celery_beat",
+    "django_celery_results",
+    "geo",
+    "votes",
+    "predict",
 ]
 
 MIDDLEWARE = [
     "django.middleware.locale.LocaleMiddleware",
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'wall.urls'
+ROOT_URLCONF = "wall.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'wall.wsgi.application'
-
+WSGI_APPLICATION = "wall.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
     "default": {
-        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
-        "NAME": os.environ.get("POSTGRES_DB", os.path.join(BASE_DIR, "db.sqlite3")),
-        "USER": os.environ.get("POSTGRES_USER", "user"),
-        "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "password"),
-        "HOST": os.environ.get("SQL_HOST", "localhost"),
-        "PORT": os.environ.get("SQL_PORT", "5432"),
+        "ENGINE":
+            os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        "NAME":
+            os.environ.get("POSTGRES_DB", os.path.join(BASE_DIR, "db.sqlite3")),
+        "USER":
+            os.environ.get("POSTGRES_USER", "user"),
+        "PASSWORD":
+            os.environ.get("POSTGRES_PASSWORD", "password"),
+        "HOST":
+            os.environ.get("SQL_HOST", "localhost"),
+        "PORT":
+            os.environ.get("SQL_PORT", "5432"),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME":
+            "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME":
+            "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME":
+            "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME":
+            "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 CELERY_TIMEZONE = TIME_ZONE
 
@@ -124,15 +132,14 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = 'static/'
-STATIC_URL = '/static/'
+STATIC_ROOT = "static/"
+STATIC_URL = "/static/"
 
-MEDIA_ROOT = 'media/'
-MEDIA_URL = '/media/'
+MEDIA_ROOT = "media/"
+MEDIA_URL = "/media/"
 
 # Celery configuration
 
@@ -151,57 +158,55 @@ EMAIL_HOST_PASSWORD = os.environ.get("SMTP_PASSWORD", "pw")
 DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL", "webmaster@localhost")
 SERVER_EMAIL = os.environ.get("SERVER_EMAIL", "root@localhost")
 
-
 CELERY_BROKER_URL = "redis://redis:6379"
 
 CELERY_RESULT_BACKEND = "django-db"
 CELERY_CACHE_BACKEND = "django-cache"
 
 JAZZMIN_SETTINGS = {
-    # title of the window
+                                                                                         # title of the window
     "site_title": _("Magic Wall Admin"),
-    # Title on the brand, and the login screen (19 chars max)
+                                                                                         # Title on the brand, and the login screen (19 chars max)
     "site_header": _("Magic Wall"),
-    # square logo to use for your site, must be present in static files, used for favicon and brand on top left
+                                                                                         # square logo to use for your site, must be present in static files, used for favicon and brand on top left
     "site_logo": "logo.png",
-    # Welcome text on the login screen
+                                                                                         # Welcome text on the login screen
     "welcome_sign": _("Magic Wall Admin"),
-    # Copyright on the footer
+                                                                                         # Copyright on the footer
     "copyright": "",
-    # The model admin to search from the search bar, search bar omitted if excluded
-    # "search_model": "blog.Article",
-    # Field name on user model that contains avatar image
+                                                                                         # The model admin to search from the search bar, search bar omitted if excluded
+                                                                                         # "search_model": "blog.Article",
+                                                                                         # Field name on user model that contains avatar image
     "user_avatar": None,
-    ############
-    # Top Menu #
-    ############
-    # Links to put along the top menu
+                                                                                         ############
+                                                                                         # Top Menu #
+                                                                                         ############
+                                                                                         # Links to put along the top menu
     "topmenu_links": [
-        # Url that gets reversed (Permissions can be added)
+                                                                                         # Url that gets reversed (Permissions can be added)
         {"name": "Home", "url": "admin:index", "permissions": ["auth.view_user"]},
-        # external url that opens in a new window (Permissions can be added)
-        # model admin to link to (Permissions checked against model)
+                                                                                         # external url that opens in a new window (Permissions can be added)
+                                                                                         # model admin to link to (Permissions checked against model)
     ],
-    #############
-    # User Menu #
-    #############
-    # Additional links to include in the user menu on the top right ("app" url type is not allowed)
+                                                                                         #############
+                                                                                         # User Menu #
+                                                                                         #############
+                                                                                         # Additional links to include in the user menu on the top right ("app" url type is not allowed)
     "usermenu_links": [],
-    #############
-    # Side Menu #
-    #############
-    # Whether to display the side menu
+                                                                                         #############
+                                                                                         # Side Menu #
+                                                                                         #############
+                                                                                         # Whether to display the side menu
     "show_sidebar": True,
-    # Whether to aut expand the menu
+                                                                                         # Whether to aut expand the menu
     "navigation_expanded": True,
-    # Hide these apps when generating side menu e.g (auth)
+                                                                                         # Hide these apps when generating side menu e.g (auth)
     "hide_apps": [],
-    # Hide these models when generating side menu (e.g auth.user)
+                                                                                         # Hide these models when generating side menu (e.g auth.user)
     "hide_models": [],
-    # List of apps (and/or models) to base side menu ordering off of (does not need to contain all apps/models)
-    "order_with_respect_to": [
-    ],
-    # Custom links to append to app groups, keyed on app name
+                                                                                         # List of apps (and/or models) to base side menu ordering off of (does not need to contain all apps/models)
+    "order_with_respect_to": [],
+                                                                                         # Custom links to append to app groups, keyed on app name
     "custom_links": {
         "books": [
             {
@@ -212,45 +217,45 @@ JAZZMIN_SETTINGS = {
             }
         ]
     },
-    # Custom icons for side menu apps/models See https://fontawesome.com/icons?d=gallery&m=free
-    # for a list of icon classes
+                                                                                         # Custom icons for side menu apps/models See https://fontawesome.com/icons?d=gallery&m=free
+                                                                                         # for a list of icon classes
     "icons": {
         "auth": "fas fa-users-cog",
         "auth.user": "fas fa-user",
         "auth.Group": "fas fa-users",
     },
-    # Icons that are used when one is not manually specified
+                                                                                         # Icons that are used when one is not manually specified
     "default_icon_parents": "fas fa-chevron-circle-right",
     "default_icon_children": "fas fa-circle",
-    #################
-    # Related Modal #
-    #################
-    # Use modals instead of popups
+                                                                                         #################
+                                                                                         # Related Modal #
+                                                                                         #################
+                                                                                         # Use modals instead of popups
     "related_modal_active": True,
-    #############
-    # UI Tweaks #
-    #############
-    # Relative paths to custom CSS/JS scripts (must be present in static files)
-    # "custom_css": "admin.css",
+                                                                                         #############
+                                                                                         # UI Tweaks #
+                                                                                         #############
+                                                                                         # Relative paths to custom CSS/JS scripts (must be present in static files)
+                                                                                         # "custom_css": "admin.css",
     "custom_js": None,
-    # Whether to show the UI customizer on the sidebar
+                                                                                         # Whether to show the UI customizer on the sidebar
     "show_ui_builder": False,
-    ###############
-    # Change view #
-    ###############
-    # Render out the change view as a single form, or in tabs, current options are
-    # - single
-    # - horizontal_tabs (default)
-    # - vertical_tabs
-    # - collapsible
-    # - carousel
-    # "changeform_format": "vertical_tabs",
-    # override change forms on a per modeladmin basis
+                                                                                         ###############
+                                                                                         # Change view #
+                                                                                         ###############
+                                                                                         # Render out the change view as a single form, or in tabs, current options are
+                                                                                         # - single
+                                                                                         # - horizontal_tabs (default)
+                                                                                         # - vertical_tabs
+                                                                                         # - collapsible
+                                                                                         # - carousel
+                                                                                         # "changeform_format": "vertical_tabs",
+                                                                                         # override change forms on a per modeladmin basis
     "changeform_format_overrides": {
-        #        "auth.user": "collapsible",
+                                                                                         #        "auth.user": "collapsible",
         "auth.group": "vertical_tabs",
     },
-    # Add a language dropdown into the admin
+                                                                                         # Add a language dropdown into the admin
     "language_chooser": True,
 }
 
